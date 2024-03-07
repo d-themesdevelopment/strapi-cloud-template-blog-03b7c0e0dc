294b7c66eb087259d157a3b9be21c4cef7b05015
dthemesdevelopment <dthemesdevelopment@gmail.com>
2024-03-07 11:01:58 -0800

    Adding web builder

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,21 @@
-module.exports = ({}) => ({});
+module.exports = ({ env }) => ({
+  "website-builder": {
+    enabled: true,
+    config: {
+      url: env("VERCEL_HOOK_URL"),
+      trigger: {
+        type: "manual",
+      },
+      body: {
+        target: {
+          ref_type: "branch",
+          type: "pipeline_ref_target",
+          ref_name: "master",
+        },
+      },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  },
+});
