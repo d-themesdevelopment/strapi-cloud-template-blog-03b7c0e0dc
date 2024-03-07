c519d095b4451f87e45222b4c125dbd6a49e594a
dthemesdevelopment <dthemesdevelopment@gmail.com>
2024-03-07 03:27:07 -0800

    updating cloud

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,20 @@
-module.exports = () => ({});
+module.exports = () => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {
+          folder: "reef-fonda",
+          use_filename: true,
+          unique_filename: false,
+        },
+        delete: {},
+      },
+    },
+  },
+});
